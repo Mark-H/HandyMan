622f7c6b9fb0b84c4ab47394f32adb0bcef7154a
Mark-H <hamstra.mark@gmail.com>
2011-03-26 05:09:30 +0100

    Improved resource listing to autom. use 1st ctx if only one exists. Added buttons for show/pub/edit/del.

--- handyman/core/actions/resourcelist.php
+++ handyman/core/actions/resourcelist.php
@@ -17,22 +17,75 @@
                 $$wantedOpt = ($options['get'][$wantedOpt]) ? $options['get'][$wantedOpt] : null;
             }
             if (!$ctx) {
-                $o = 'Please choose a context.';
-                $o .= '<ul data-inset="true" data-role="listview">';
-                $o .= $this->processActions($this->listContexts($modx));
-                $o .= '</ul>';
+                $contexts = $this->listContexts($modx);
+                if (count($contexts) > 1) {
+                    $o = 'Please choose a context.';
+                    $o .= '<ul data-inset="true" data-role="listview">';
+                    $o .= $this->processActions($contexts);
+                    $o .= '</ul>';
+                }
+                elseif (count($contexts) == 1) {
+                    $ctx = $contexts[0]['object']->get('key');
+                }
+                else {
+                    return 'Your contexts are messed up.';
+                }
             } 
             
-            else {
-                if ($parent == 0) {
-
+            if ($ctx) {
+                if ($parent > 0) {
+                    $current = $modx->getObject('modResource',$parent);
+                    $o = '<h2>'.$current->get('pagetitle').'</h2>';
+                    $resEditMap = array(
+                        array (
+                            'action' => 'res_details',
+                            'linktext' => 'Show details',
+                            'linkparams' => array(
+                                'ctx' => $ctx,
+                                'rid' => $parent
+                            )
+                        ),
+                        array(
+                            'action' => 'res_publish',
+                            'linktext' => 'Publish',
+                            'linkparams' => array(
+                                'ctx' => $ctx,
+                                'rid' => $parent
+                            )
+                        ),
+                        array(
+                            'action' => 'res_modify',
+                            'linktext' => 'Modify',
+                            'linkparams' => array(
+                                'ctx' => $ctx,
+                                'rid' => $parent
+                            )
+                        ),
+                        array(
+                            'action' => 'res_delete',
+                            'linktext' => 'Delete',
+                            'linkparams' => array(
+                                'ctx' => $ctx,
+                                'rid' => $parent
+                            )
+                        )
+                    );
+                    $o .= '<ul data-inset="true" data-role="listview">';
+                    $o .= $this->processActions($resEditMap);
+                    $o .= '</ul>';
+                    $o .= '<h2>Children Resources</h2>';
+                } else {
+                    $parent = 0;
+                    $o .= '<h2>Resources</h2>';
                 }
-                elseif ($parent > 0) {
-                    $o = 'Viewing children of resource '.$parent.'.';
+                $subResources = $this->listResources($modx,$ctx,$parent);
+                if (count($subResources) > 0) {
+                    $o .= '<ul data-inset="true" data-role="listview">';
+                    $o .= $this->processActions($this->listResources($modx,$ctx,$parent));
+                    $o .= '</ul>';
+                } else {
+                    $o .= '<p>This resource does not have any children.</p>';
                 }
-                $o .= '<ul data-inset="true" data-role="listview">';
-                $o .= $this->processActions($this->listResources($modx,$ctx,$parent));
-                $o .= '</ul>';
             }
             
             return $o;
@@ -49,13 +102,14 @@
                 $contexts[] = array(
                     'action' => 'resourcelist',
                     'linktext' => $ctx->get('key'),
-                    'linkparams' => array('ctx' => $ctx->get('key'))
+                    'linkparams' => array('ctx' => $ctx->get('key')),
+                    'object' => $ctx
                 );
             }
             return $contexts;
         }
         
-        public function listResources(&$modx,$ctx,$parent) {
+        public function listResources(&$modx,$ctx,$parent = 0) {
             $c = $modx->newQuery('modResource');
             $c->where(array(
                 'context_key' => $ctx,
@@ -67,7 +121,8 @@
                 $resources[] = array(
                     'action' => 'resourcelist',
                     'linktext' => $res->get('pagetitle'),
-                    'linkparams' => array('ctx' => $ctx, 'parent' => $res->get('id'))
+                    'linkparams' => array('ctx' => $ctx, 'parent' => $res->get('id')),
+                    'object' => $ctx
                 );
             }
             return $resources;
