4cfa21813064c3e299dd8fc91c5214ae79f9da6e
Mark Hamstra <hello@markhamstra.com>
2011-10-03 15:23:33 +0200

    Basic (visual) textile tests

---
+++ _build/tests/textile/index.php
@@ -0,0 +1,83 @@
+<?php
+/* JUST FOR TESTING THE TEXTILE IMPLEMENTATION */
+/* Instantiate constants and variables for later use.
+ ***/
+define('HANDYMAN', true);
+$hmo = '';
+
+/* Include the main HandyMan class.
+ * This class takes care of authentification and provides the extension
+ * with functions to execute the requests.
+ *
+ * After inclusion, set up the $hm variable as the main object.
+ **/
+include_once dirname(dirname(dirname(dirname(__FILE__)))).'/handyman/core/classes/handyman.class.php';
+$hm = new HandyMan;
+$hm->initialize();
+
+$hm->modx->getService('h2t','html2textile',$hm->config['corePath'].'classes/textile/');
+$hm->modx->getService('t2h','Textile',$hm->config['corePath'].'classes/textile/');
+
+$case = (!empty($_REQUEST['case'])) ? $_REQUEST['case'] : 1;
+
+$original = file_get_contents('data/'.$case.'.html');
+
+$textiled = $hm->modx->h2t->detextile($original);
+
+$html = $hm->modx->t2h->TextileThis($textiled);
+
+$amts = 25;
+$i = 0;
+$recursingTextile = $textiled;
+$recursingHtml = $html;
+while ($i < $amts) {
+    $recursingTextile = $hm->modx->h2t->detextile($recursingHtml);
+    $recursingHtml = $hm->modx->t2h->TextileThis($recursingTextile);
+    $i++;
+}
+
+$entTextiled = htmlentities($textiled);
+$entTextiledRecursive = htmlentities($recursingTextile);
+echo <<<HTML
+<table border=1>
+    <tr>
+        <th colspan="2">Original</th>
+        <th colspan="2">To Textile + To HTML</th>
+        <th colspan="2">Recursive $amts times</th>
+    </tr>
+    <tr>
+        <td colspan="2">
+            $original
+        </td>
+        <td colspan="2">
+            $html
+        </td>
+        <td colspan="2">
+            $recursingHtml
+        </td>
+    </tr>
+    <tr>
+        <th colspan="3">Textile 1</th>
+        <th colspan="3">Textile $amts</th>
+    </tr>
+    <tr>
+        <td colspan="3">
+            <pre>$entTextiled</pre>
+        </td>
+        <td colspan="3">
+            <pre>$entTextiledRecursive</pre>
+        </td>
+    </tr>
+</table>
+HTML;
+
+echo <<<CSS
+<style type="text/css">
+    pre {
+    white-space: pre-line;
+    }
+</style>
+CSS;
+
+
+?>
